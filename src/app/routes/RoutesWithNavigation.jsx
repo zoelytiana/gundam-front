56f8b0cdb423be5facc0e06b8f22150c72428520
Zoely <zralamboarisoa@gmail.com>
2021-10-27 10:29:12 +0200

    modification couleur de fond du site

--- src/app/routes/RoutesWithNavigation.jsx
+++ src/app/routes/RoutesWithNavigation.jsx
@@ -29,7 +29,7 @@ const RoutesWithNavigation = () => {
 
     return (
         <BrowserRouter>
-            <div className="min-h-full flex flex-col items-center justify-center bg-white cursor-default">
+            <div className="min-h-full flex flex-col items-center justify-center bg-gray-200 cursor-default">
                 { isLogged && <IdleTimerCustom />}
                 <Navbar/>
                 <main>
