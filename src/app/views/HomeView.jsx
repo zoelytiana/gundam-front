f4a32a5192e95dac613cdbc660b554ffee34a163
Amanda Lan-Cheong <amanda.lan-cheong@gmail.com>
2021-10-28 11:14:51 +0200

    style global, change in index.css, add btn

--- src/app/views/HomeView.jsx
+++ src/app/views/HomeView.jsx
@@ -4,7 +4,9 @@ const HomeView = () => {
     return (
         <div className="container">
             <div className="max-w-7xl px-4 mx-auto">
-               HOME 
+                <div className="btn-form">
+                    <button><div className="btn-form-text">HOME</div></button>
+                </div>
             </div>
         </div>
     )
