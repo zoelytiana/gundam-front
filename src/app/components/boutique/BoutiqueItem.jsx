438ddfe4362f85c30915c8be7fc33b78f4b66c5b
Amanda Lan-Cheong <amanda.lan-cheong@gmail.com>
2021-11-03 23:06:02 +0100

    tailwind modification

--- src/app/components/boutique/BoutiqueItem.jsx
+++ src/app/components/boutique/BoutiqueItem.jsx
@@ -7,27 +7,27 @@ import cart from "./../../assets/images/cart.png";
 function boutiqueItem(props) {
     return (
         <div className="boutiqueCard">
-            <ul className="flex flex-wrap justify-center ">
+            <ul className="grid grid-cols-3">
                 {props.gundams.slice(0, 9).map(gundam => {
                     return (
-                        <li className="m-px" key={gundam.id}>
+                        <li className="m-4" key={gundam.id}>
                             <div className="box-product-bottom w-72">
                                 <div className="box-product h-72">
                                     <img className="w-48" src={gundam.image} alt={gundam.name} />
                                 </div>
 
                             </div>
-                            <div className="shadow-sm rounded m-1">
+                            <div className="shadow-sm rounded m-1 mt-2 p-2">
                                 <div className="flex flex-col items-center">
                                     <h4 className="font-bold">{gundam.name}</h4>
                                     <p>{gundam.price}€</p>
                                 </div>
-                                <div className="boutiqueButton">
-                                    <Link to={URL_DETAIL}>
-                                        <button><img src={view} alt="" /></button>
+                                <div className="flex justify-end items-center">
+                                    <Link className="ml-1" to={URL_DETAIL}>
+                                        <button><img className="w-6 h-6" src={view} alt="" /></button>
                                     </Link>
-                                    <Link to={URL_PANIER}>
-                                        <button><img className="w-8" src={cart} alt="" /></button>
+                                    <Link className="ml-2" to={URL_PANIER}>
+                                        <button><img className="w-6 h-6" src={cart} alt="" /></button>
                                     </Link>
                                 </div>
                             </div>
--- src/app/components/boutique/PanierItem.jsx
+++ src/app/components/boutique/PanierItem.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import classes from "./PanierItem.module.css"
+import classes from "./PanierItem.module.css";
+import visa from "./../../assets/images/mode-de-paiement_0000_Calque3.jpg";
+import paypal from "./../../assets/images/mode-de-paiement_0001_Calque2.jpg";
+import mastercard from "./../../assets/images/mode-de-paiement_0002_Calque1.jpg";
+import info from "./../../assets/images/icon_info.png";
 
 function PanierItem(props) {
     return (
         <div>
             <h3>Mon panier ( 3 articles )</h3>
-            <div className="flex">
+            <div className="lg:flex justify-around">
                 <div>
                     {props.gundams.map(gundam => {
                         return (
-                            <div className="flex bg-white rounded p-1 my-11">
+                            <div className="xl:flex lg:justify-center lg:items-center bg-white rounded p-8 m-11">
                                 <img src={gundam.image} alt={gundam.name} />
                                 <div className="p-0.5">
                                     <h4>{gundam.name}</h4>
@@ -19,40 +23,50 @@ function PanierItem(props) {
                         )
                     })}
                 </div>
-                <div className="bg-white rounded p-1 mb-11">
-                    <p className={classes.freeDelivery}>Pour tout commande supérieur à 150€, vous pouvez bénéficier de la livraison gratuite</p>
-                    <p>Total</p>
-                    <div>
+                <div className="bg-white rounded p-8 m-11">
+                    <p className="bg-primary-200 p-8">Pour tout commande supérieur à 150€, vous pouvez bénéficier de la livraison gratuite</p>
+                    <h3 className="mt-4 mb-4">Total</h3>
+                    <div className="flex justify-between mb-8">
                         <p>Sous total</p>
                         <p>450€</p>
                     </div>
-                    <div>
+                    <div className="flex justify-between mb-2">
                         <p>livraison</p>
                         <p>0€</p>
                     </div>
                     <hr />
-                    <div>
+                    <div className="flex justify-between mt-4">
                         <p>Total TVA incluse</p>
                         <p>450€</p>
                     </div>
-                    <button className="btn-form"><p className="btn-form-text">Procéder au paiment</p></button>
-                    <button className="btn-form"><p className="btn-form-text">Continuer mes achats</p></button>
+                    <button className="btn-form m-4">
+                        <p className="btn-form-text w-28">Procéder au paiment</p>
+                    </button>
+                    <button className="btn-form m-4">
+                        <p className="btn-form-text w-28">Continuer mes achats</p>
+                    </button>
                 </div>
             </div>
-            <button>Instruction de livraison</button>
-            <div className={classes.PaiementInfos}>
-                <h3 className={classes.accept}>
+            <div className="flex ml-8">
+                <img className="mr-2" src={info} alt="info" />
+                <button><h3>Instruction de livraison</h3></button>
+            </div>
+
+            <div className="text-center lg:mx-40">
+                <h3 className="mb-4">
                     Nous acceptons
                 </h3>
-                <div className={classes.PaiementMode}>
-                    <p>visa</p>
-                    <p>PayPal</p>
-                    <p>Mastercard</p>
+                <div className="flex justify-center bg-white p-8">
+                    <img src={visa} alt="visa" />
+                    <img src={paypal} alt="paypal" />
+                    <img src={mastercard} alt="mastercard" />
                 </div>
-                <h3>Vous avez un compte Amazon ?</h3>
-                <p>Connectez-vous avec votre compte amazon et essayer la commande Express</p>
-                <button className={classes.amazon}>amazon pay</button>
-                <p>Utilisez cotre compte amazon</p>
+                <div className="bg-white mt-4 p-8">
+                    <h3>Vous avez un compte Amazon ?</h3>
+                    <p>Connectez-vous avec votre compte amazon et essayer la commande Express</p>
+                    <button className="bg-primary-300 border-solid border-2 border-black rounded pr-4 pl-4">amazon pay</button>
+                    <p>Utilisez cotre compte amazon</p>
+                </div>
             </div>
         </div>
     )
