dc744fdaec5ba03368cced8795667bdec00aaf7e
Amanda Lan-Cheong <amanda.lan-cheong@gmail.com>
2021-11-03 15:37:50 +0100

    text box

--- src/app/components/boutique/BoutiqueItem.jsx
+++ src/app/components/boutique/BoutiqueItem.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { URL_DETAIL, URL_PANIER } from './../../shared/constants/urls/urlConstants';
-import classes from "./BoutiqueItem.module.css"
+import view from "./../../assets/images/view.png";
+import cart from "./../../assets/images/cart.png";
 
 function boutiqueItem(props) {
     return (
-        <div className={classes.boutiqueCard}>
+        <div className="boutiqueCard">
             <ul className="flex flex-wrap justify-center ">
                 {props.gundams.slice(0, 9).map(gundam => {
                     return (
                         <li className="m-px" key={gundam.id}>
-                            <img src={gundam.image} alt={gundam.name} />
-                            <div className={classes.boutiqueInfos}>
+                            <div className="box-product-bottom w-28">
+                                <div className="box-product h-28">
+                                    <img src={gundam.image} alt={gundam.name} />
+                                </div>
+
+                            </div>
+                            <div className="shadow-sm rounded m-1">
                                 <div className="flex flex-col items-center">
                                     <h4 className="font-bold">{gundam.name}</h4>
                                     <p>{gundam.price}€</p>
                                 </div>
-                                <div className={classes.boutiqueButton}>
+                                <div className="boutiqueButton">
                                     <Link to={URL_DETAIL}>
-                                        <button><img className={classes.buttonCart} src="./../../assets/images/view.png" alt="" /></button>
+                                        <button><img src={view} alt="" /></button>
                                     </Link>
                                     <Link to={URL_PANIER}>
-                                        <button><img className={classes.buttonCart} src="./../../assets/images/cart.png" alt="" /></button>
+                                        <button><img className="w-8" src={cart} alt="" /></button>
                                     </Link>
                                 </div>
                             </div>
