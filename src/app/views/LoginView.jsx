329a8e01d2c7203c858d287d1bcb77aeca47a22b
Amanda Lan-Cheong <amanda.lan-cheong@gmail.com>
2021-11-15 11:57:13 +0100

    modal function

--- src/app/views/LoginView.jsx
+++ src/app/views/LoginView.jsx
@@ -5,23 +5,28 @@ import { authenticate } from './../api/backend/account';
 import { URL_HOME } from './../shared/constants/urls/urlConstants';
 import { isAuthenticated } from './../shared/services/accountServices';
 import Login from './../components/account/Login';
-
+// import Modal from "./../components/account/Modal";
 /**
  * View/Page Login
  * 
  * @param {object} history 
  * @author Peter Mollet
  */
- const LoginView = ({ history }) => {
+const LoginView = ({ history }) => {
 
     const [errorLog, setErrorLog] = useState(false)
     const dispatch = useDispatch()
 
+    // const [openModal, setOpenModal] = useState(false);
+    // const toggleModal = () => {
+    //     setOpenModal(true)
+    // }
+
     const handleLogin = (values) => {
         authenticate(values).then(res => {
-            if(res.status === 200 && res.data.id_token) {
+            if (res.status === 200 && res.data.id_token) {
                 dispatch(signIn(res.data.id_token))
-                if(isAuthenticated) history.push(URL_HOME)
+                if (isAuthenticated) history.push(URL_HOME)
             }
         }).catch(() => setErrorLog(true))
     }
@@ -30,6 +35,8 @@ import Login from './../components/account/Login';
         <div className="container">
             <div className="max-w-7xl mx-auto">
                 <Login submit={handleLogin} errorLog={errorLog} />
+                {/* <button onClick={() => toggleModal()} >Mot de passe Oublié?</button>
+                <Modal toggleModal={openModal} /> */}
             </div>
         </div>
     );
