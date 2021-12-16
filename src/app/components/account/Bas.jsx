d86bc90a543b685d1ab082ca9b91a886b30fa2e1
Zoely <zralamboarisoa@gmail.com>
2021-12-16 16:57:38 +0100

    sg conflit

--- src/app/components/account/Bas.jsx
+++ src/app/components/account/Bas.jsx
@@ -11,33 +11,12 @@ const change = () => {
 
 const Bas = () => {
 
-<<<<<<< HEAD
     return ( 
         <div className = 'flex flex-wrap justify-around h-1 p-4' >
             <a href = '/amazon' onClick = { change() } > < img src = { Amazon } alt = 'Amazon' /> </a> 
             <a href = '/livraison' onClick = { change() } > < img src = { Livraison } alt = 'Livraison' /> </a>  
             <a href = '/quality' onClick = { change() } > < img src = { Qualite } alt = 'Qualite' /> </a> 
             <a href = 'service' onClick = { change() } > < img  src = { Service } alt = 'Service' /> </a> 
-=======
-    return (
-        < div className='flex-bas ' >
-
-            <a href='/amazon'
-                onClick={change()} > < img src={Amazon}
-                    alt='Amazon' /> </a>
-            <a href='/livraison'
-                onClick={change()} > < img src={Livraison}
-                    alt='Livraison' /> </a>
-            <a href='/paiement'
-                onClick={change()} > < img src={Paiement}
-                    alt='Paiement' /> </a>
-            <a href='/quality'
-                onClick={change()} > < img src={Qualite}
-                    alt='Qualite' /> </a>
-            <a href='service'
-                onClick={change()} > < img src={Service}
-                    alt='Service' /> </a>
->>>>>>> 0a3f7c947a4f71c39f0104f75187ec6541e907ca
         </div>
     )
 }
