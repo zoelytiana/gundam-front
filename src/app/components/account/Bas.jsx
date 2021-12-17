ad025b0a1e31138dbe09d0016053a256c0bbd528
Amanda Lan-Cheong <amanda.lan-cheong@gmail.com>
2021-12-17 09:51:15 +0100

    dev pull

--- src/app/components/account/Bas.jsx
+++ src/app/components/account/Bas.jsx
@@ -11,14 +11,6 @@ const change = () => {
 
 const Bas = () => {
 
-<<<<<<< HEAD
-    return ( 
-        <div className = 'flex flex-wrap justify-around h-1 p-4' >
-            <a href = '/amazon' onClick = { change() } > < img src = { Amazon } alt = 'Amazon' /> </a> 
-            <a href = '/livraison' onClick = { change() } > < img src = { Livraison } alt = 'Livraison' /> </a>  
-            <a href = '/quality' onClick = { change() } > < img src = { Qualite } alt = 'Qualite' /> </a> 
-            <a href = 'service' onClick = { change() } > < img  src = { Service } alt = 'Service' /> </a> 
-=======
     return (
         < div className='flex-bas ' >
 
@@ -37,7 +29,6 @@ const Bas = () => {
             <a href='service'
                 onClick={change()} > < img src={Service}
                     alt='Service' /> </a>
->>>>>>> 0a3f7c947a4f71c39f0104f75187ec6541e907ca
         </div>
     )
 }
