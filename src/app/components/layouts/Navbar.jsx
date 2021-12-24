fe72a22fe96290ae0d84d830d1897f5fd7a3dfc6
Zoely <zralamboarisoa@gmail.com>
2021-12-24 13:18:31 +0100

    header burger menu

--- src/app/components/layouts/Navbar.jsx
+++ src/app/components/layouts/Navbar.jsx
@@ -33,13 +33,13 @@ const Navbar = () => {
 
                 <div className="w-full h-px bg-white my-1"></div>
 
-                <div className="flex justify-between items-center">
-                    <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div className="flex justify-between">
+                    <div className="w-1/4 block flex-grow lg:flex lg:items-center lg:w-auto">
                         <MenuBtn/>
                     </div>
 
                     
-                    <div className="flex items-center justify-end flex-1 lg:w-0">
+                    <div className="w-1/2 block flex justify-end flex-1 lg:w-0">
                         <MenuContact/>
                     </div>
                 </div>
@@ -51,26 +51,47 @@ const Navbar = () => {
 export default Navbar
 
 const MenuBtn = () => {
-    return (
-        <div className="uppercase text-white font-bold text-sm lg:flex-grow">
-            <Link to={URL_PRESENTATION}>
-                <div className='block lg:inline-block text-white lg:mt-0 hover:text-white mr-4 float-left link'>
-                    Presentation
-                </div>
-            </Link>
-            <Link to={URL_BOUTIQUE}>
-                <div className='block lg:inline-block lg:mt-0 text-white hover:text-white mr-4 float-left link'>
-                    Boutique
-                </div>
-            </Link>
-            <Link to={URL_PROMOTION}>
-                <div className='block lg:inline-block lg:mt-0 text-white hover:text-white mr-4 float-left link'>
-                    Promotion
-                </div>
-            </Link>
-            
 
-        </div>
+    const loadMenu = () => {
+        document.getElementById("nav-content").classList.toggle("hidden");
+    }
+
+    return (
+
+        <nav className="flex justify-start flex-wrap p-1 w-1/2 uppercase text-white font-bold text-sm lg:flex-grow">
+
+            <div className="block lg:hidden">
+                <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white" onClick={()=>loadMenu()}>
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+
+            <div className=" flex-grow lg:flex lg:w-auto hidden lg:block pt-6 lg:pt-0" id="nav-content">
+                <ul className="list-reset lg:flex flex-1">
+                    <li className="mr-3">
+                    <Link to={URL_PRESENTATION}>
+                            <div className="block lg:inline-block text-white lg:mt-0 hover:text-white mr-4 link">
+                                Presentation
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="mr-3">
+                    <Link to={URL_BOUTIQUE}>
+                            <div className="block lg:inline-block lg:mt-0 text-white hover:text-white mr-4 link">
+                                Boutique
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="mr-3">
+                    <Link to={URL_PROMOTION}>
+                            <div className="block lg:inline-block lg:mt-0 text-white hover:text-white mr-4 link">
+                                Promotion
+                            </div>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+</nav>       
     )
 }
 
@@ -78,6 +99,7 @@ const ConnectionBtn = () => {
     const isLogged = useSelector(selectIsLogged)
     console.log('islogged :',isLogged)
     const dispatch = useDispatch()
+
     if(isLogged)
     return(
         <>
@@ -108,25 +130,47 @@ const MenuContact = () => {
     const isLogged = useSelector(selectIsLogged)
     console.log('islogged :',isLogged)
     const dispatch = useDispatch()
+    const loadMenu2 = () => {
+        document.getElementById("nav-content2").classList.toggle("hidden");
+    }
+   
     if(isLogged)
     return (
         <>
+        <nav className="flex justify-end flex-wrap p-1 w-full text-white font-bold text-sm lg:flex-grow">
 
-        <Link to={URL_ACCOUNT}>
-                <div className='link text-white px-4 hover:text-white'>
-                    Mon compte
-                </div>
-        </Link>
-            <Link to={URL_ENVIE}>
-            <div className='block lg:inline-block lg:mt-0 text-white hover:text-white mr-4 float-left link'>
-                Liste d'envie
+        <div className="block lg:hidden">
+                <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white" onClick={()=>loadMenu2()}>
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
             </div>
-            </Link>
-            <Link to={URL_CONTACT}>
-            <div className='block lg:inline-block lg:mt-0 text-white hover:text-white mr-4 float-left link'>
-                Contact
+
+            <div className="w-full flex-grow lg:flex lg:w-auto hidden lg:block pt-6 lg:pt-0" id="nav-content2">
+                <ul className="list-reset lg:flex flex-1">
+                <li className="mr-3">
+                <Link to={URL_ACCOUNT}>
+                        <div className='block lg:inline-block link text-white  mr-4 hover:text-white'>
+                            Mon compte
+                        </div>
+                </Link>
+                </li>
+                <li className="mr-3">
+                    <Link to={URL_ENVIE}>
+                    <div className='block lg:inline-block lg:mt-0 text-white hover:text-white mr-4 link'>
+                        Liste d'envie
+                    </div>
+                    </Link>
+                </li>
+                <li className="mr-3">
+                    <Link to={URL_CONTACT}>
+                    <div className='block lg:inline-block lg:mt-0 text-white hover:text-white mr-4 link'>
+                        Contact
+                    </div>
+                    </Link>
+                </li>
+            </ul>
             </div>
-            </Link>
+    </nav> 
         </>
     ) 
     else return(
@@ -137,3 +181,5 @@ const MenuContact = () => {
         </Link>
     )
 }
+
+
